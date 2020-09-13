3c907e46012c753417c2f1a574544d101b24c50b
Dominic Fournier <dominicfournier@outlook.com>
2020-09-13 15:37:03 -0400

    Added Core

---
+++ components/alert.js
@@ -0,0 +1,24 @@
+import Container from "./container";
+import cn from "classnames";
+import { EXAMPLE_PATH } from "../lib/constants";
+
+export default function Alert() {
+  return (
+    <div className={cn("border-b")}>
+      <Container>
+        <div className="py-2 text-center text-sm">
+          <>
+            Vous souhaitez contribuer ? Nous acceptons les soumissions sur&nbsp;
+            <a
+              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+              className="underline hover:text-success duration-200 transition-colors"
+            >
+              GitHub
+            </a>
+            .
+          </>
+        </div>
+      </Container>
+    </div>
+  );
+}
